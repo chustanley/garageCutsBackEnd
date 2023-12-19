4c44d37885f167da9baaab455387efa944eb9b56
Stanley Chu <chustanleys@gmail.com>
2023-12-19 00:55:57 -0800

    added SSL

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
 
 const conversationRoute = require("./routes/conversations");
 const messageRoute = require("./routes/messages");
@@ -12,9 +15,7 @@ dotenv.config();
 
 const cors = require("cors"); // Import the cors package
 
-app.use(cors());
-
-console.log("HELLO?");
+// app.use(cors());
 
 app.use(express.json());
 
@@ -33,6 +34,18 @@ mongoose
 app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(8800, () => {
-  console.log("Backend server is running!");
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+  },
+  app,
+);
+
+// app.listen(8800, () => {
+//   console.log("Backend server is running!");
+// });
+
+sslServer.listen(3443, () => {
+  console.log("Backend server is running on port 3443");
 });
